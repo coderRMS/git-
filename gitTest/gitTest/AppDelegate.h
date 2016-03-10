5609ed6a678982f0dc3e8db4143114c1f530567b
rms <rms_coder@163.com>
2016-03-10 11:47:53 +0800

    初始化

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 阮明森 on 16/3/10.
+//  Copyright © 2016年 rms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
